8f346916f6b39082a18d6559992cedcde5310eab
Spark <79936503+spa5k@users.noreply.github.com>
2025-11-29 15:17:32 +0530

    updated deps

--- crates/kagzi-proto/build.rs
+++ crates/kagzi-proto/build.rs
@@ -1,5 +1,5 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::configure()
+    tonic_prost_build::configure()
         .file_descriptor_set_path("src/descriptor.bin")
         .compile_protos(&["../../proto/kagzi.proto"], &["../../proto"])?;
     Ok(())
